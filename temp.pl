e5d80acfab6ef54d04cef63187419f40e26da90b
irfansofyana <irfansofyana0305@gmail.com>
2018-11-19 12:28:18 +0700

    add revision rules and facts

--- temp.pl
+++ temp.pl
@@ -16,6 +16,8 @@ stamina(100).
 armor(0).
 
 /*temporary rules */
-w :- retract(playerposition(X, Y)), Next_x is X-1, asserta(playerposition(Next_x, Y)).
-s :- retract(playerposition(X, Y)), Next_x is X+1, asserta(playerposition(Next_x, Y)).
-d :- retract(playerposition(X, Y)), Next_y is Y+1, asserta(playerposition(X, Next_y)).
+w :- inc, retract(playerposition(X, Y)), Next_y is Y-1, asserta(playerposition(X, Next_Y)).
+s :- inc, retract(playerposition(X, Y)), Next_x is X+1, asserta(playerposition(Next_x, Y)).
+e :- inc, retract(playerposition(X, Y)), Next_y is Y+1, asserta(playerposition(X, Next_y)).
+n :- inc, retract(playerposition(X, Y)), Next_x is X-1, asserta(playerposition(Next_x, Y)).
+
