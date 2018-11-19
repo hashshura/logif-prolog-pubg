977dbc75dc7c45d5dd4b18a0331cf802528cac6e
irfansofyana <irfansofyana0305@gmail.com>
2018-11-19 12:35:43 +0700

    hasil gabung

--- temp.pl
+++ temp.pl
@@ -23,49 +23,49 @@ inc :-
 	asserta(step(Next_X)).
 
 start :-
-	write("======================================================="), nl,
-	write("=                         _             _             ="), nl,
-	write("=                        | |           ( )            ="), nl,
-	write("=         _ __  _ __ ___ | | ___   __ _|/ ___         ="), nl,
-	write("=        | '_ \\| '__/ _ \\| |/ _ \\ / _` | / __|        ="), nl,
-	write("=        | |_) | | | (_) | | (_) | (_| | \\__ \\        ="), nl,
-	write("=        | .__/|_|  \\___/|_|\\___/ \\__, | |___/        ="), nl,
-	write("=        | |                       __/ |              ="), nl,
-	write("=        |_|    unknown           |___/               ="), nl,
-	write("=                  battlegrounds                      ="), nl,
-	write("======================================================="), nl,
+	write('======================================================='), nl,
+	write('=                         _             _             ='), nl,
+	write('=                        | |           ( )            ='), nl,
+	write('=         _ __  _ __ ___ | | ___   __ _|/ ___         ='), nl,
+	write('=        | \'_ \\| \'__/ _ \\| |/ _ \\ / _` | / __|        ='), nl,
+	write('=        | |_) | | | (_) | | (_) | (_| | \\__ \\        ='), nl,
+	write('=        | .__/|_|  \\___/|_|\\___/ \\__, | |___/        ='), nl,
+	write('=        | |                       __/ |              ='), nl,
+	write('=        |_|    unknown           |___/               ='), nl,
+	write('=                  battlegrounds                      ='), nl,
+	write('======================================================='), nl,
 	nl,
 	nl,
-	write(" Welcome, Warrior."), nl,
+	write(' Welcome, Warrior.'), nl,
 	nl,
-	write(" You are chosen as one of class K-3's representatives"), nl,
-	write(" for the battle by a(n) (un)lucky lottery. Carve the"), nl,
-	write(" way out through your opponents' corpses."), nl,
+	write(' You are chosen as one of class K-3\'s representatives'), nl,
+	write(' for the battle by a(n) (un)lucky lottery. Carve the'), nl,
+	write(' way out through your opponents\' corpses.'), nl,
 	nl,
-	write(" Available commands:                               "), nl,
-	write("    start. -- start the game!                      "), nl,
-	write("    help. -- show available commands               "), nl,
-	write("    quit. -- quit the game                         "), nl,
-	write("    look. -- look around you                       "), nl,
-	write("    n. s. e. w. -- move                            "), nl,
-	write("    map. -- look at the map and detect enemies     "), nl,
-	write("    take(Object). -- pick up an object             "), nl,
-	write("    drop(Object). -- drop an object                "), nl,
-	write("    use(Object). -- use an object                  "), nl,
-	write("    attack. -- attack enemy that crosses your path "), nl,
-	write("    status. -- show your status                    "), nl,
-	write("    save(Filename). -- save your game              "), nl,
-	write("    load(Filename). -- load previously saved game  "), nl,
+	write(' Available commands:                               '), nl,
+	write('    start. -- start the game!                      '), nl,
+	write('    help. -- show available commands               '), nl,
+	write('    quit. -- quit the game                         '), nl,
+	write('    look. -- look around you                       '), nl,
+	write('    n. s. e. w. -- move                            '), nl,
+	write('    map. -- look at the map and detect enemies     '), nl,
+	write('    take(Object). -- pick up an object             '), nl,
+	write('    drop(Object). -- drop an object                '), nl,
+	write('    use(Object). -- use an object                  '), nl,
+	write('    attack. -- attack enemy that crosses your path '), nl,
+	write('    status. -- show your status                    '), nl,
+	write('    save(Filename). -- save your game              '), nl,
+	write('    load(Filename). -- load previously saved game  '), nl,
 	nl,
-	write(" Legends:           "), nl,
-	write("    W = weapon      "), nl,
-	write("    A = armor       "), nl,
-	write("    M = medicine    "), nl,
-	write("    O = ammo        "), nl,
-	write("    P = player      "), nl,
-	write("    E = enemy       "), nl,
-	write("    - = accessible  "), nl,
-	write("    X = inaccessible"), nl,
+	write(' Legends:           '), nl,
+	write('    W = weapon      '), nl,
+	write('    A = armor       '), nl,
+	write('    M = medicine    '), nl,
+	write('    O = ammo        '), nl,
+	write('    P = player      '), nl,
+	write('    E = enemy       '), nl,
+	write('    - = accessible  '), nl,
+	write('    X = inaccessible'), nl,
 	nl.
 
 deadzone(X, Y) :-
@@ -83,11 +83,11 @@ map :-
 	
 printmap(X, Y) :-
 	(Y == 21, !, nl, Next_X is X + 1, printmap(Next_X, 1));
-	(X < 21, !, write(" "), (
-		(playerposition(X, Y), write("P"), !); 
-		(deadzone(X, Y), write("X"), !);
-		write("_")
-	), write(" "), Next_Y is Y + 1, printmap(X, Next_Y));
+	(X < 21, !, write(' '), (
+		(playerposition(X, Y), write('P'), !); 
+		(deadzone(X, Y), write('X'), !);
+		write('_')
+	), write(' '), Next_Y is Y + 1, printmap(X, Next_Y));
 	X == 21.
 
 /*temporary rules */
