0bd5d0bf0d4b98bbaff73464cecf33b8cb33d8e7
irfansofyana <irfansofyana0305@gmail.com>
2018-11-19 12:23:32 +0700

    add some rules and facts

---
+++ temp.pl
@@ -0,0 +1,21 @@
+/* temporary facts */ 
+:- dynamic(health/1).
+:- dynamic(stamina/1).
+:- dynamic(armor/1).
+:- dynamic(playerposition/2).
+:- dynamic(weapon/1).
+:- dynamic(equip/1).
+:- dynamic(enemyposition/3).
+:- dynamic(enemycount/1).
+:- dynamic(exist/3).
+:- dynamic(inventory/1).
+
+health(100).
+playerposition(0,0).
+stamina(100).
+armor(0).
+
+/*temporary rules */
+w :- retract(playerposition(X, Y)), Next_x is X-1, asserta(playerposition(Next_x, Y)).
+s :- retract(playerposition(X, Y)), Next_x is X+1, asserta(playerposition(Next_x, Y)).
+d :- retract(playerposition(X, Y)), Next_y is Y+1, asserta(playerposition(X, Next_y)).
