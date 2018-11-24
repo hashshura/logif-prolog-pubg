9349083997e0ab103b66bce6796ad76bdacb45d7
Asif Hummam Rais <hashshura@gmail.com>
2018-11-25 01:57:33 +0700

    Decrease enemiesleft from death by deadzone

--- games.pl
+++ games.pl
@@ -34,9 +34,17 @@ inc :-
 	checkvictory.
 	
 enemydeadzone(Id) :-
-	(enemyposition(Id, X, Y), deadzone(X, Y), retract(enemyposition(Id, X, Y)), !; 1 == 1),
+	(
+		enemyposition(Id, X, Y), deadzone(X, Y), retract(enemyposition(Id, X, Y)),
+		retract(enemiesleft(ELeft)), ENext is ELeft-1, asserta(enemiesleft(ENext)),
+		write('An enemy has been hurled to death by the agonizingly fast deadzone hurricane!'), nl, !;
+		1 == 1
+	),
 	Next_Id is Id + 1, enemycount(Enemy_count),
-	(Next_Id =< Enemy_count, enemydeadzone(Next_Id), !; 1 == 1).
+	(
+		Next_Id =< Enemy_count, enemydeadzone(Next_Id), !;
+		1 == 1
+	).
 	
 /*Enemies stuffs*/
 spawnenemies :-
